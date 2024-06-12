ea1e3ba67f7a1d0c032b6ba53091e3867f4399b1
Andrzej Gorgonzola <andrzej.gorzala@hotmail.co.uk>
2024-06-12 13:34:53 +0100

    Film Career changes
    JScript animation complete
    Sec-2 created

--- Assets/Film/film.js
+++ Assets/Film/film.js
@@ -1,16 +1,16 @@
 // Wrap every letter in a span
-var textWrapper = document.querySelector('.head');
+var textWrapper = document.querySelector('.ml3');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
 anime.timeline({loop: true})
   .add({
-    targets: '.head .letter',
+    targets: '.ml3 .letter',
     opacity: [0,1],
     easing: "easeInOutQuad",
     duration: 2250,
     delay: (el, i) => 150 * (i+1)
   }).add({
-    targets: '.head',
+    targets: '.ml3',
     opacity: 0,
     duration: 1000,
     easing: "easeOutExpo",
